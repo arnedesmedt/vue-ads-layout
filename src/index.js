c0210a0538a036a51e30460256654302d31460e8
Arne De Smedt <arnedesmdt@gmail.com>
2018-09-18 15:58:08 +0200

    Add components to index

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import VueAdsLayout from './components/Layout';
+import VueAdsBar from './components/Bar';
+import VueAdsDrawer from './components/Drawer';
+import VueAdsMinify from './components/buttons/Minify';
+import VueAdsMenu from './components/buttons/Menu';
+
+export {
+    VueAdsLayout,
+    VueAdsBar,
+    VueAdsDrawer,
+    VueAdsMinify,
+    VueAdsMenu,
+};
